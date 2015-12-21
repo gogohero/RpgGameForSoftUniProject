432f48d74e4c8e30649b55f396d9b1b8619f98e4
gogohero <nrggig@gmail.com>
2015-12-22 00:24:44 +0200

    povment changes

--- Hero3TrialMono/Hero3TrialMono/Map.cs
+++ Hero3TrialMono/Hero3TrialMono/Map.cs
@@ -11,13 +11,14 @@ namespace Hero3TrialMono
 {
     public class Map:State
     {
+        private int spritePosition = 1;
         private Texture2D background;
         private Texture2D playerImage;
         private Rectangle backgroundViewRectengel;
         private SpriteBatch PlayerBatch;
         private Rectangle playerViewRectangle;
         private Vector2 playerPosition;
-        private Warrior go6oWarrior = new Warrior(new Vector2(1000, 1000), 0, "go6o");
+        private Warrior go6oWarrior = new Warrior(new Vector2(-200, -300), 0, "go6o");
         
         public Map()
         {
@@ -28,16 +29,16 @@ namespace Hero3TrialMono
         {
              background = Engine.ContentLoader.Content.Load<Texture2D>("Images/mapOne");
             playerImage = Engine.ContentLoader.Content.Load<Texture2D>("Images/Champion");
-            playerViewRectangle = new Rectangle(0, 0,100,100);
+            playerViewRectangle = new Rectangle(0,0,100,150);
             backgroundViewRectengel = new Rectangle(0, 0, 3200, 3200);
             playerPosition = go6oWarrior.Position;
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, null, null, null, null, Matrix.CreateTranslation((float)(playerPosition.X), (float)(playerPosition.Y), 0));
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, null, null, null, null, Matrix.CreateTranslation((float)(playerPosition.X+800), (float)(playerPosition.Y+650), 0));
 
-            spriteBatch.Draw(background, Vector2.Zero, backgroundViewRectengel, Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 1);
+            spriteBatch.Draw(background, new Vector2(-800,-650), backgroundViewRectengel, Color.White, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, 1);
             spriteBatch.Draw(playerImage, go6oWarrior.Position,playerViewRectangle, Color.White, 0,playerPosition, new Vector2(1, 1), SpriteEffects.None, 0);
             KeyboardState KBS = Keyboard.GetState();
             //Moving
@@ -56,17 +57,22 @@ namespace Hero3TrialMono
 
             int speed = 30;
 
-            if (KBS.IsKeyDown(Keys.Right))
-            {   
-                
+            if (KBS.IsKeyDown(Keys.Right) && playerPosition.X>-1800)
+            {
+                if (spritePosition > 800)
+                {
+                    spritePosition = 1;
+                }
+                playerViewRectangle.X = spritePosition;
                 playerPosition.X -= speed;
+                spritePosition += 200;
             }
-            else if (KBS.IsKeyDown((Keys.Left)))
+            else if (KBS.IsKeyDown(Keys.Left)&& playerPosition.X<-20)
             {
                 playerPosition.X += speed;
             }
 
-            if (KBS.IsKeyDown(Keys.Down) && playerPosition.Y > -3020)
+            if (KBS.IsKeyDown(Keys.Down) && playerPosition.Y > -2000)
                 {
                     
 
@@ -74,29 +80,29 @@ namespace Hero3TrialMono
 
                     
             }
-            else if (KBS.IsKeyDown(Keys.Up))
+            else if (KBS.IsKeyDown(Keys.Up) && playerPosition.Y < -20)
             {
 
 
                 playerPosition.Y += speed;
 
             }
-            if (KBS.IsKeyDown(Keys.Right) && KBS.IsKeyDown(Keys.Down))
+            if (KBS.IsKeyDown(Keys.Right) && KBS.IsKeyDown(Keys.Down) && playerPosition.Y > -2000)
             {
                 playerPosition.Y -= speed / 2;
                 playerPosition.X -= 0;
             }
-            if (KBS.IsKeyDown(Keys.Up) && KBS.IsKeyDown((Keys.Left)))
+            if (KBS.IsKeyDown(Keys.Up) && KBS.IsKeyDown((Keys.Left)) && playerPosition.Y < -20)
             {
                 playerPosition.Y += speed / 2;
                 playerPosition.X += 0;
             }
-            if (KBS.IsKeyDown(Keys.Down) && KBS.IsKeyDown((Keys.Left)))
+            if (KBS.IsKeyDown(Keys.Down) && KBS.IsKeyDown((Keys.Left)) && playerPosition.Y > -2000)
             {
                 playerPosition.Y -= speed / 2;
                 playerPosition.X += 0;
             }
-            if (KBS.IsKeyDown(Keys.Up) && KBS.IsKeyDown((Keys.Right)))
+            if (KBS.IsKeyDown(Keys.Up) && KBS.IsKeyDown((Keys.Right)) && playerPosition.Y < -20)
             {
                 playerPosition.Y += speed / 2;
                 playerPosition.X -= 0;
